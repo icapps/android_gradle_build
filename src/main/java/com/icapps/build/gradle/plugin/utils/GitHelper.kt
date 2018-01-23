9538bb409a100e7121018df3774592cd8d8a81ba
Koen Van Looveren <koen.vanlooveren@icapps.com>
2018-01-23 14:09:41 +0100

    Git get latest commit messages since other branche

--- src/main/java/com/icapps/build/gradle/plugin/utils/GitHelper.kt
+++ src/main/java/com/icapps/build/gradle/plugin/utils/GitHelper.kt
@@ -64,15 +64,28 @@ object GitHelper {
         return !branchExists(branch)
     }
 
-    fun getLatestCommitMessages(): LinkedList<String> {
+    fun getLatestCommitMessages(sinceBranch: String): LinkedList<String> {
         val messages = LinkedList<String>()
         val rt = Runtime.getRuntime()
-        val pr = rt.exec("git log --pretty=format:\"%s\" --no-merges")
+        val commitHash = getLatestCommitHash(sinceBranch)
+        val pr = rt.exec("git log $commitHash.. --pretty=format:\"%s\" --no-merges")
         val input = BufferedReader(InputStreamReader(pr.inputStream))
         for (line in input.lines()) {
             messages.add(line)
         }
         input.close()
         return messages
+    }
+
+    private fun getLatestCommitHash(branch: String): String {
+        val rt = Runtime.getRuntime()
+        val pr = rt.exec("git log -n 1 $branch --pretty=format:\"%H\"")
+        val input = BufferedReader(InputStreamReader(pr.inputStream))
+        val output = StringBuilder()
+        for (line in input.lines()) {
+            output.append(line)
+        }
+        input.close()
+        return output.toString()
     }
 }
